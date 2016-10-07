1dbcd9e27bf810ae070a7e9156c1d46f3aff5339
Lance Wicks <lw@judocoach.com>
2016-10-07 22:18:51 +0500

    Initial commit, remote control via node

---
+++ ddr1.js
@@ -0,0 +1,21 @@
+var net = require('net');
+
+var client = new net.Socket();
+client.connect({port: '5951', host:'192.168.2.80'}, function() {
+	console.log('Connected');
+	client.write("<shortcut name='main_b_row_named_input' value='ddr1' />\n<shortcut name='main_auto'/>\n" );
+	client.destroy();
+});
+
+client.on('data', function(data) {
+	console.log('Received: ' + data);
+	client.destroy(); // kill client after server's response
+});
+
+client.on('close', function() {
+	console.log('Connection closed');
+});
+
+client.on('error', function(err) {
+   console.log(err)
+})
