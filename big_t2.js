3a1bc23cce6883c6224248968ca8b33bbc8d7dbd
Lance Wicks <lw@judocoach.com>
2016-10-08 17:12:59 +0500

    Working web based prototype added
    remote.html making ajax calls via JS to remote_server.js

---
+++ big_t2.js
@@ -0,0 +1,21 @@
+var net = require('net');
+
+var client = new net.Socket();
+client.connect({port: '5951', host:'192.168.2.80'}, function() {
+	console.log('Connected');
+	client.write("<shortcut name='v7_b_row_named_input' value='7' />\n<shortcut name='v7_auto'/>\n" );
+	client.destroy();
+});
+
+client.on('data', function(data) {
+	console.log('Received: ' + data);
+	client.destroy(); // kill client after server's response
+});
+
+client.on('close', function() {
+	console.log('Connection closed');
+});
+
+client.on('error', function(err) {
+   console.log(err)
+})
